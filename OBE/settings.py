e8671c7727bb2b561c63cedb38704214bf709928
Waseem-786 <82876411+Waseem-786@users.noreply.github.com>
2024-02-05 12:49:37 +0500

    Update settings.py

--- OBE/settings.py
+++ OBE/settings.py
@@ -191,5 +191,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'w4w.w4me@gmail.com'
-EMAIL_HOST_PASSWORD = 'pynb dhwp piuy onpj'
+EMAIL_HOST_USER = 'your gamil address'
+EMAIL_HOST_PASSWORD = 'your password'
