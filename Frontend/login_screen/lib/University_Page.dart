f672632a691773ee9a5bcdde5996ddd2d34a57f3
hashir4825 <hashir4825@gmail.com>
2024-04-18 19:29:09 +0500

    I added the fetch university functionality

--- Frontend/login_screen/lib/University_Page.dart
+++ Frontend/login_screen/lib/University_Page.dart
@@ -1,38 +1,98 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:login_screen/Create_University.dart';
-import 'package:login_screen/Custom_Widgets/Custom_Text_Field.dart';
-import 'Custom_Widgets/Custom_Button.dart';
-import 'Custom_Widgets/Custom_Text_Style.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class University_Page extends StatelessWidget{
+import 'package:login_screen/Create_University.dart';
+import 'package:login_screen/Custom_Widgets/Custom_Button.dart';
+
+class University_Page extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
+  State<University_Page> createState() => _University_PageState();
+}
+
+class _University_PageState extends State<University_Page> {
+  final storage = FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      encryptedSharedPreferences: true,
+    ),
+  );
+  @override
+  void initState() {
+    super.initState();
+    storage.read(key: "access_token").then((accessToken) {
+      fetchUniversities(accessToken!);
+    });
+  }
+  List<dynamic> universities = [];
+
+  Future<void> fetchUniversities(String accessToken) async {
+    print(accessToken);
+    final response = await http.get(
+      Uri.parse('http://192.168.0.103:8000/api/university'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+    print("response.body");
+    if (response.statusCode == 200) {
+      print("Success");
+
+      // all the data of the users is stored in responseData
+      universities = jsonDecode(response.body);
+      // Handle the response data
+      print("Hellog");
+      print(universities);
+
+      setState(() {}); // Refresh the UI after fetching data
+    } else {
+      throw Exception('Failed to load universities');
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xffc19a6b),
         title: Center(
           child: Text(
             'University Page',
-            style: CustomTextStyles.headingStyle(fontSize: 20),
+            style: TextStyle(fontSize: 20),
           ),
         ),
-
       ),
-      body: Center(
-        child: Container(
-        child:   Custom_Button(
-            onPressedFunction: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Create_University()));
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: universities.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(universities[index]['name']),
+                  ),
+                );
               },
-            ButtonText: 'Add University',
-          ButtonWidth: 200,
+            ),
           ),
-        ),
+          Center(
+            child: Container(
+              child: Custom_Button(
+                onPressedFunction: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Create_University()),
+                  );
+                },
+                ButtonText: 'Add University',
+                ButtonWidth: 200,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
